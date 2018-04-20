547da93db2a672afbdeb030926536c22bf97f5e9
ricardo.fahham <ricardo.fahham@corp.globo.com>
2018-04-20 11:25:55 -0300

    Adicionando novos projetos: cor, radar e musica

---
+++ tocando musica/tocando_musica.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Código
+
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('sunset.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
+
+
+# Biblioteca
+
+# print('{}'.format())
+# variável = int(input(''))
+# variável = float(input(''))
+# variável = str(input(''))
+
+
+# Instalando o pygame
+# $ brew install mercurial
+# $ brew install sdl sdl_image sdl_mixer sdl_ttf smpeg portmidi
+# $ pip install hg+http://bitbucket.org/pygame/pygame
