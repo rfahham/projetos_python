cd899e16006b8595cd839735dd90bf95bf5934b9
ricardo.fahham <ricardo.fahham@g.globo>
2022-12-03 15:36:30 -0300

    Jogo de perguntas

--- pergunta.py
+++ pergunta.py
@@ -2,26 +2,32 @@
 # -*- coding: utf-8 -*-
 
 
-print '''
+print ('''
+========================
 Quem descobriu o Brasil?
+========================
 
-Selecione a resposta correta:
+Selecione a opção correta:
 
 (a) Rui Barbosa
 (b) Marechal Deodoro da Fonseca
 (c) Pedro Alvares Cabral
-'''
+
+''')
 
 while True:
-    resposta = raw_input('R: ')
+    resposta = input('Opção: ')
 
     if resposta == 'a':
-        print 'Você errou'
+        print ('Você errou!')
+        print ()
 
     elif resposta == 'b':
-        print 'você errou' 
+        print ('você errou!' )
+        print ()
                       
     elif resposta == 'c':  
-        print 'Você acertou, Parabéns !!!'
+        print ('Você acertou! Parabéns !!!')
+        print ()
         break
 
