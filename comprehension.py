15af37a21c64c588b8ecc6d26a3e51ec9040a4fa
ricardo.fahham <ricardo.fahham@g.globo>
2022-12-03 15:49:39 -0300

    Trabalhando com comprehension

--- comprehension.py
+++ comprehension.py
@@ -1,16 +1,21 @@
-comprehension
+print('=============================')
+print('Trabalhando com Comprehension')
+print('=============================')
+print()
 
 #List
-S = "Ricardo"
+S = "Ricardo Fahham"
 [i for i in S]
 
 # Set
-S = "Ricardo"
+S = "Ricardo Fahham"
 {i for i in S}
 
+
 # Dictionary
-S = "Ricardo"
+S = "Ricardo Fahham"
 {i: ord(i) for i in S}
+
 
 
 
