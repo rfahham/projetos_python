ed53392ed427193370d26d0d5d9dea40afa4c884
ricardo.fahham <ricardo.fahham@g.globo>
2023-04-03 12:54:27 -0300

    avaliandoo investimento gado ou soja

---
+++ investimento.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+# soja -> R$162/saca - 60k
+# soja -> produtividade = 3000k/ha
+
+# Gado  -> produtividade = 26 arrobas/ha
+# 1 arroba = 15k
+# preço do arroba = R$ 240,00
+
+print('-_'*22)
+print('Avaliando investimento')
+print('-_'*22)
+print('')
+
+terra = 1000
+prod_soja = 3000
+preco_soja = 162 / 60
+faturamento_soja = terra * prod_soja * preco_soja
+
+prod_gado = 26
+preco_gado = 275
+faturamento_gado = terra * prod_gado * preco_gado
+
+# print(f'{faturamento_soja:,}')
+# print(f'{faturamento_gado:,}')
+
+if faturamento_soja > faturamento_gado:
+    print("Investir em soja")
+else:
+    print("Investir em gado")
