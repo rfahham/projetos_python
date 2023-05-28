0b8a875c82da6849b68bcb9de6175402a18df887
Ricardo Fahham <rfahham@hotmail.com>
2023-05-28 10:39:17 -0300

    adicionando chave em arquivo

--- temperatura/main.py
+++ temperatura/main.py
@@ -1,14 +1,16 @@
 import requests
+from key import key_api # pega o valor da variável "key_api" que está no arquivo key.py
 
-key = input("Digite chave: ")
+# key = input("Digite chave: ")
 cidade = input("Digite a cidade: ")
 pais = "br"
 unidade = "metric"
-link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade},{pais}&units={unidade}&lang=ptbr&APPID={key}"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade},{pais}&units={unidade}&lang=ptbr&APPID={key_api}"
 
 # print(link)
 
 requisicao = requests.get(link)
+print("Status Code: ", requisicao)
 requisicao_dics = requisicao.json()
 descricao = requisicao_dics['weather'][0]['description']
 temperatura = requisicao_dics['main']['temp']
