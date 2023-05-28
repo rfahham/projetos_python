07ea1bf49a50745fc6e5de1e2413b88af69e9469
Ricardo Fahham <rfahham@hotmail.com>
2023-05-28 09:51:00 -0300

    adicionando inputs key e cidade

--- temperatura/main.py
+++ temperatura/main.py
@@ -1,10 +1,12 @@
 import requests
 
-key = "9c67f7fd97e570204447648b096f437b"
-cidade = "Petropolis"
+key = input("Digite chave: ")
+cidade = input("Digite a cidade: ")
 pais = "br"
 unidade = "metric"
-link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade},{pais}&unit={unidade}&lang=ptbr&APPID={key}"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade},{pais}&units={unidade}&lang=ptbr&APPID={key}"
+
+# print(link)
 
 requisicao = requests.get(link)
 requisicao_dics = requisicao.json()
@@ -12,6 +14,8 @@ descricao = requisicao_dics['weather'][0]['description']
 temperatura = requisicao_dics['main']['temp']
 sensacao_termica = requisicao_dics['main']['feels_like']
 
-print(f"A temperatura em {cidade} é de {temperatura}")
+print("-"*50)
+print(f"A temperatura em {cidade} é de {temperatura} graus")
 print(f"A condição do tempo é: {descricao}")
-print(f"A sensação térmica é de: {sensacao_termica}")
+print(f"A sensação térmica é de: {sensacao_termica} graus")
+print("-"*50)
