9c617ca7f10f221c1c54d3aa4512ad9d64584800
Ricardo Fahham <rfahham@hotmail.com>
2023-08-02 14:31:41 -0300

    apagando venv

--- compara.py
+++ compara.py
@@ -1,11 +1,12 @@
 # Compara duas strings
+# Neste caso tudo que vem depois do HTTP vai dar erro
 
-string1 = "Orquestração de containers com Kubernetes"
+string1 = "http://www.globo.com"
 
-string2 = "Orquestração de containers sem Kubernetes"
+string2 = "https://www.globo.com"
 
 conjunto = enumerate(zip(string1, string2))
-print(list(conjunto))
+# print(list(conjunto))
 
 for indice, (a, b) in conjunto:
     if a != b:
