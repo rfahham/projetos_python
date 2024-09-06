46283057b4a2320c23b4bcba8be1d05b51eed566
Ricardo Fahham <rfahham@hotmail.com>
2024-09-06 08:53:31 -0300

    atualizando código input

--- questionario.py
+++ questionario.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
+print("-"*50)
+print("Clínica !!!")
+print("-"*50)
+print("")
 
 def clinic():
-    print "Voce acabou de entrar na clinica!"
-    print "Voce entra pela porta a esquerda (left) ou a direita (right)?"
-    answer = raw_input("Digite left (esquerda) ou right (direita) e pressione 'Enter'.").lower()
+    
+    print("Seja Bem-Vindo!!!")
+    print("Você acabou de entrar na clinica!")
+    print("Você entra pela porta a esquerda (left) ou a direita (right)?")
+
+    answer = input("Digite left (esquerda) ou right (direita) e pressione 'Enter'. ").lower()
+    
     if answer == "left" or answer == "l":
-        print "Esta e a sala de Abuso Verbal, seu monte de caca de papagaio!"
+        print("Esta é a sala de Abuso Verbal, seu monte de caca de papagaio!")
     elif answer == "right" or answer == "r":
-        print "E claro que esta e a Sala das Discussoes. Eu ja disse isso!"
+        print("É claro que esta é a 'Sala das Discussões'. Eu já disse isso!")
     else:
-        print "Voce nao escolheu esquerda ou direita. Tente de novo."
+        print("Você não escolheu esquerda ou direita. Tente de novo.")
         clinic()
 
 clinic()
